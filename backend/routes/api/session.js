985397382de17ca38e7503e53534369fcd86c16a
kevin <kchase07@gmail.com>
2024-08-01 10:47:33 -0500

    added express validator

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -11,13 +11,28 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Restore session user
+router.get('/', (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
 // backend/routes/api/session.js
 // ...
 
 // Log in
 router.post(
-    '/',
-    async (req, res, next) => {
+  '/',
+  async (req, res, next) => {
       const { credential, password } = req.body;
 
       const user = await User.unscoped().findOne({
@@ -59,5 +74,7 @@ router.delete(
     return res.json({ message: 'success' });
   }
 );
+
+
 
 module.exports = router;
